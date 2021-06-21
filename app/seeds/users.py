fb8b4db1b9381a00d11a74e275eae720848614b6
Andrew Moss <andrewmossaudio@gmail.com>
2021-06-21 18:52:08 -0500

    pw hash added to user seed

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -3,18 +3,24 @@ from app.models import db, User
 
 # Adds a demo user, you can add other users here if you want
 def seed_users():
-
-    demo = User(username='Demo', email='demo@aa.io',
-                hashed_password='password', full_name="Demo User", cash_balance=10000000)
+    hp = generate_password_hash("password")
+    demo = User(
+        username="Demo",
+        email="demo@aa.io",
+        hashed_password=hp,
+        full_name="Demo User",
+        cash_balance=10000000,
+    )
 
     db.session.add(demo)
 
     db.session.commit()
+
 
 # Uses a raw SQL query to TRUNCATE the users table.
 # SQLAlchemy doesn't have a built in function to do this
 # TRUNCATE Removes all the data from the table, and resets
 # the auto incrementing primary key
 def undo_users():
-    db.session.execute('TRUNCATE users RESTART IDENTITY CASCADE;')
+    db.session.execute("TRUNCATE users RESTART IDENTITY CASCADE;")
     db.session.commit()
