875f9ea5fe0620a4cf663a564ed134215dced013
Andrew Moss <andrewmossaudio@gmail.com>
2021-06-21 18:15:48 -0500

    del extras

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,12 +3,11 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import LoginForm from "./components/auth/LoginForm";
 import SignUpForm from "./components/auth/SignUpForm";
-import NavBar from "./components/NavBar";
+import NavBar from "./components/NavBar/NavBar";
 import ProtectedRoute from "./components/auth/ProtectedRoute";
-import UsersList from "./components/UsersList";
-import User from "./components/User";
+import User from "./components/Users/User";
 import { authenticate } from "./store/session";
-import Planet from './components/Planet_Description/planetDescription'
+import Planet from './components/PlanetDescription/PlanetDescription'
 
 function App() {
   // const [authenticated, setAuthenticated] = useState(false);
@@ -39,9 +38,6 @@ function App() {
         <Route path="/sign-up" exact={true}>
           <SignUpForm />
         </Route>
-        <ProtectedRoute path="/users" exact={true}>
-          <UsersList />
-        </ProtectedRoute>
         <ProtectedRoute path="/users/:userId" exact={true}>
           <User />
         </ProtectedRoute>
