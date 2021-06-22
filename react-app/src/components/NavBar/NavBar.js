3541bcd0fdda259fe93c1315412fa4ba348a2f66
Zaviar Brown <brown.zaviar@gmail.com>
2021-06-22 00:05:30 -0700

    nav work in progress

--- react-app/src/components/NavBar/NavBar.js
+++ react-app/src/components/NavBar/NavBar.js
@@ -1,37 +1,31 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from '../auth/LogoutButton';
+import { useSelector } from 'react-redux';
 
 const NavBar = () => {
-  return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink to="/" exact={true} activeClassName="active">
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/login" exact={true} activeClassName="active">
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/sign-up" exact={true} activeClassName="active">
-            Sign Up
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/users" exact={true} activeClassName="active">
-            Users
-          </NavLink>
-        </li>
-        <li>
-          <LogoutButton />
-        </li>
-      </ul>
-    </nav>
-  );
+    const sessionUser = useSelector(state => state.session.user);
+    let setLinks;
+
+    if (sessionUser) {
+        setLinks = (
+            <LogoutButton />
+        )
+    } else {
+        setLinks = (
+            <>
+                <NavLink to="/login" exact={true} activeClassName="active">Login</NavLink>
+                <NavLink to="/sign-up" exact={true} activeClassName="active">Sign Up</NavLink>
+            </>
+        );
+    }
+
+    return (
+        <div>
+            <NavLink to="/" exact={true} activeClassName="active">Home</NavLink>
+            {setLinks}
+        </div>
+    );
 }
 
 export default NavBar;
