362a68fce28ea1b56ebe8b64fdeb8957236915a5
Andrew Moss <andrewmossaudio@gmail.com>
2021-06-23 16:03:43 -0500

    strrting user cash factoring

--- react-app/src/components/Transaction/Transaction.js
+++ react-app/src/components/Transaction/Transaction.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { editOneAsset, getAllAssets, deleteOneAsset, createOneAsset } from '../../store/assets';
+import { createATransaction, getAllTransactions } from '../../store/transactions';
 
 import styles from './Transaction.module.css';
 
@@ -18,6 +19,7 @@ export default function Transaction({ planetId }) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    let assetPrice = 5 //! place holder for the price we grab from algorithm
     let asset = Object.values(assets)
     let number
     let found = asset.find((el) => el['planetId'] === +planetId && el['userId'] === +userId) ? asset.find((el) => el['planetId'] === +planetId && el['userId'] === +userId) : null;
@@ -27,7 +29,7 @@ export default function Transaction({ planetId }) {
           dispatch(deleteOneAsset(found.id))
           dispatch(getAllAssets())
           //! dispatch delete
-          //!delete because they are selling all their shares
+          //! delete because they are selling all their shares
           return
         }
         else if (amount > found.shares) {
@@ -41,17 +43,20 @@ export default function Transaction({ planetId }) {
 
       } else if (orderType === 'buy') {
         number = amount * 1
-        // if (cash balance is enough) //!THIS MEANS ORDER TYPE IS BUY
+        let transPrice = number * assetPrice
+        //! if (cash balance is enough) //! THIS MEANS ORDER TYPE IS BUY
         dispatch(editOneAsset(found.id, number))
+        dispatch(createATransaction(transPrice, +planetId, number))
         dispatch(getAllAssets())
       }
 
 
     } else { //! IF FOUND IS set to NULL---------
       if (orderType === 'buy') {
-        // new asset needs to be created
         number = amount * 1
+        let transPrice = number * assetPrice
         dispatch(createOneAsset(number, +planetId))
+        dispatch(createATransaction(transPrice, +planetId, number))
         dispatch(getAllAssets())
       } else {
         if (orderType === 'sell') {
@@ -67,7 +72,8 @@ export default function Transaction({ planetId }) {
 
   useEffect(() => {
     dispatch(getAllAssets());
-  }, [dispatch]);
+    dispatch(getAllTransactions())
+  }, [amount]);
 
   //! create conditional rendering also using ternaries and state to
   //! handle either BUY or SELL
