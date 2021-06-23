e6009052650ed4a0acedc7606412231b498ab413
Ryan <DaltonR121@gmail.com>
2021-06-23 18:09:34 -0400

    trying to push news script again

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,7 @@ from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
 from .api.planet_routes import planet_routes
 from .api.transaction_routes import transaction_routes
+from .api.article_routes import article_routes
 
 
 from .seeds import seed_commands
@@ -36,6 +37,7 @@ app.register_blueprint(user_routes, url_prefix="/api/users")
 app.register_blueprint(auth_routes, url_prefix="/api/auth")
 app.register_blueprint(planet_routes, url_prefix="/api/planet")
 app.register_blueprint(transaction_routes, url_prefix="/api/transactions")
+app.register_blueprint(article_routes, url_prefix="/api/article")
 db.init_app(app)
 Migrate(app, db)
 
