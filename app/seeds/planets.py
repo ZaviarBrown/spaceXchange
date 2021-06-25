84b3ca94720ad2ec09fc7eaead41c316a62f528c
Zaviar Brown <brown.zaviar@gmail.com>
2021-06-25 16:19:39 -0700

    planet leader and top export started

--- app/seeds/planets.py
+++ app/seeds/planets.py
@@ -8,7 +8,9 @@ def seed_planets():
         price=0.242524,
         ticker="MCRY",
         description="The closest planet to our sun and the smallest planet in our Solar System. Due to the abundance of iron ore, Mercury has primarily turned into a material mining planet.",
-        labor_force=5000000,
+        labor_force=5120178,
+        planet_leader="",
+        top_export="",
         crypto="dogecoin",
     )
 
@@ -18,6 +20,8 @@ def seed_planets():
         ticker="VNUS",
         description="With a surface temperature of 462°C (864°F), Venus holds the title of hottest planet in our Solar System. Since these are uninhabitable conditions for humans, the planet is completely run by AI machines.",
         labor_force=0,
+        planet_leader="",
+        top_export="",
         crypto="theta-fuel",
     )
 
@@ -26,7 +30,9 @@ def seed_planets():
         price=2.39,
         ticker="MARS",
         description="The red planet. First planet to be colonized by Earth. After generations of incredible developmental progress, Mars has a thriving economy and has earned a reputation of 'Earth 2.0'.",
-        labor_force=150000000,
+        labor_force=1520231881,
+        planet_leader="",
+        top_export="",
         crypto="leo-token",
     )
 
@@ -36,6 +42,8 @@ def seed_planets():
         ticker="JPTR",
         description="At 317 times the mass of Earth, Jupiter reigns supreme as the most massive planet in our Solar System. Due to being a gas giant with incredibly violent storms, Jupiter's inhabitants actually live scattered across its 79 moons.",
         labor_force=5000000,
+        planet_leader="",
+        top_export="",
         crypto="0x",
     )
 
@@ -45,6 +53,8 @@ def seed_planets():
         ticker="STRN",
         description="Saturn's rings are its most recognizable feature, and have become a tourist attraction for space travelers. However, since Saturn is a gas giant, the space hotels are all located on Saturn's 82 moons",
         labor_force=5000000,
+        planet_leader="",
+        top_export="",
         crypto="cardano",
     )
 
@@ -54,6 +64,8 @@ def seed_planets():
         ticker="URNS",
         description="Despite being a gas giant, Uranus is the coldest planet in our Solar System and has been dubbed an ice giant. The Uranus workforce is mostly located on the 27 moons that orbit it.",
         labor_force=5000000,
+        planet_leader="",
+        top_export="",
         crypto="algorand",
     )
 
@@ -63,15 +75,19 @@ def seed_planets():
         ticker="NPTN",
         description="The last planet in the Solar System and the last planet to be colonized, it shares the gas giant/ice giant label with Uranus. However, having an Earth-sized solid core allows for living beings to inhabit this planet to harvest its precious elements.",
         labor_force=5000000,
+        planet_leader="",
+        top_export="",
         crypto="klay-token",
     )
 
-     = Planet(
-        name="",
+    amerind = Planet(
+        name="Amerind",
         price=,
         ticker="",
-        description="",
+        description="Amerind was an inhabited planet said to be half a galaxy away from Earth, which was nonetheless discovered in the 23rd century to contain settlements of pre-industrial Humans, the Amerind inhabitants.",
         labor_force=,
+        planet_leader="",
+        top_export="",
         crypto="",
     )
 
@@ -81,6 +97,8 @@ def seed_planets():
         ticker="",
         description="",
         labor_force=,
+        planet_leader="",
+        top_export="",
         crypto="",
     )
 
@@ -90,6 +108,8 @@ def seed_planets():
         ticker="",
         description="",
         labor_force=,
+        planet_leader="",
+        top_export="",
         crypto="",
     )
 
@@ -99,6 +119,8 @@ def seed_planets():
         ticker="",
         description="",
         labor_force=,
+        planet_leader="",
+        top_export="",
         crypto="",
     )
 
@@ -108,6 +130,8 @@ def seed_planets():
         ticker="",
         description="",
         labor_force=,
+        planet_leader="",
+        top_export="",
         crypto="",
     )
 
@@ -117,6 +141,8 @@ def seed_planets():
         ticker="",
         description="",
         labor_force=,
+        planet_leader="",
+        top_export="",
         crypto="",
     )
 
@@ -126,6 +152,8 @@ def seed_planets():
         ticker="",
         description="",
         labor_force=,
+        planet_leader="",
+        top_export="",
         crypto="",
     )
 
@@ -135,6 +163,8 @@ def seed_planets():
         ticker="",
         description="",
         labor_force=,
+        planet_leader="",
+        top_export="",
         crypto="",
     )
 
@@ -144,6 +174,8 @@ def seed_planets():
         ticker="",
         description="",
         labor_force=,
+        planet_leader="",
+        top_export="",
         crypto="",
     )
 
@@ -153,6 +185,8 @@ def seed_planets():
         ticker="",
         description="",
         labor_force=,
+        planet_leader="",
+        top_export="",
         crypto="",
     )
 
@@ -162,6 +196,8 @@ def seed_planets():
         ticker="",
         description="",
         labor_force=,
+        planet_leader="",
+        top_export="",
         crypto="",
     )
 
@@ -171,6 +207,8 @@ def seed_planets():
         ticker="",
         description="",
         labor_force=,
+        planet_leader="",
+        top_export="",
         crypto="",
     )
 
@@ -180,6 +218,8 @@ def seed_planets():
         ticker="",
         description="",
         labor_force=,
+        planet_leader="",
+        top_export="",
         crypto="",
     )
 
@@ -190,6 +230,19 @@ def seed_planets():
     db.session.add(saturn)
     db.session.add(uranus)
     db.session.add(neptune)
+    db.session.add(amerind)
+    db.session.add()
+    db.session.add()
+    db.session.add()
+    db.session.add()
+    db.session.add()
+    db.session.add()
+    db.session.add()
+    db.session.add()
+    db.session.add()
+    db.session.add()
+    db.session.add()
+    db.session.add()
 
     db.session.commit()
 
--- projNotes.md
+++ projNotes.md
@@ -2,13 +2,13 @@ Possible features
 
     * Assets page
     * Transactions page
-    * Stories scraped by python, displayed on our site - DONE
-    * Host our own API - DONE
     * Fractional shares
     * Live updating chart with Plotly.js
+    * "Get a job button" give you a random amount of money at increment
+    * Stories scraped by python, displayed on our site - DONE
+    * Host our own API - DONE
 
 BUGS THAT NEED TO BE FIXED
 
-    - Signup fucking doesn't work
-    - Buying power only updates on refresh
-    - Need to give new users cash balance
+    - Need to be able to deposit for buying power
+    - Dashboard "owned assets" is stuck at a weird spot on the page
