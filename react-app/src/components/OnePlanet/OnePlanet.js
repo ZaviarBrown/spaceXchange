65d5d23b52fd0f9508fd225e2cf09e10f0134276
John Sims <jwsims816@gmail.com>
2021-06-25 21:35:28 -0500

    tradeable assets includes live price

--- react-app/src/components/OnePlanet/OnePlanet.js
+++ react-app/src/components/OnePlanet/OnePlanet.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import styles from './OnePlanet.module.css';
 
-export default function OnePlanet({ planet }) {
+export default function OnePlanet({ planet, prices }) {
+
   return (
     <>
       <div className={styles.onePlanet}>
         <div>{planet.name}</div>
         <div>{planet.ticker}</div>
+        <div>Price:</div>
+        <div>{prices[planet.name.toLowerCase()]?.price || "fetching..."}</div>
       </div>
     </>
   );
