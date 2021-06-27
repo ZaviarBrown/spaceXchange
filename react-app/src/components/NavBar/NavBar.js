ef204fb9a2d4edf3027132ae12ea13f3716fa3cd
Ryan <DaltonR121@gmail.com>
2021-06-27 07:02:48 -0400

    added search bar functionality and styled

--- react-app/src/components/NavBar/NavBar.js
+++ react-app/src/components/NavBar/NavBar.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from '../auth/LogoutButton';
 import { useSelector } from 'react-redux';
+import { getAllPlanets } from '../../store/planet';
 import searchIcon from '../../assets/outline_search_white_24dp.png';
 import styles from './NavBar.module.css';
 
 const NavBar = () => {
+    const dispatch = useDispatch();
+    
+    const [search, setSearch] = useState('');
+    const planets = useSelector((state) => Object.values(state.planet));
+    console.log(planets);
     const sessionUser = useSelector(state => state.session.user);
     let setLinks;
 
@@ -35,6 +42,31 @@ const NavBar = () => {
         );
     }
 
+    useEffect(() => {
+        dispatch(getAllPlanets());
+        }, []);
+
+    const filterPlanets = (planets, search) => {
+        if (!search) {
+            return planets;
+        }
+
+        return planets.filter((planetName) => {
+            let data = [];
+            let lowerSearch = search.toLowerCase()
+            const planet = planetName.name.toLowerCase();
+            const ticker = planetName.ticker.toLowerCase();
+            data.push(planet)
+            data.push(ticker)
+
+            return data.includes(lowerSearch) || ticker.includes(lowerSearch)
+        })
+    }
+
+    const searchFilter = filterPlanets(planets, search)
+
+    console.log()
+
     return (
         <div className={styles.navContainer}>
             <div className={styles.navA}>
@@ -44,8 +76,24 @@ const NavBar = () => {
                 </div>
             <div className={styles.searchBar}>
                 <div className={styles.inputContainer}>
-                    <input type="text" />
+                    <form action='/' method='GET'>
+                        <input type="text" 
+                        value={search}
+                        onChange={((e) => setSearch(e.target.value))}/>
                     <img src={searchIcon} className={styles.searchIcon} />
+                    </form>
+                    <div className={styles.searchList}>
+                        <ul> {(search.length > 0) ? 
+                            searchFilter.map((planet) => (
+                                <div className={styles.searchItem}>
+                                    <NavLink to={`/planet/${planet.id}`}>
+                                        <li key={planet.id} onClick={(e) => setSearch('')}>{ planet.name }</li>
+                                    </NavLink>
+                                </div>
+                            ))
+                        : null }
+                        </ul>
+                    </div>
                 </div>
             </div>
             {setLinks}
