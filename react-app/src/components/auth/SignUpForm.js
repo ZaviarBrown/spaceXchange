4ed1380e5a9f7c431453e9bef8864ed2080decaa
Zaviar Brown <brown.zaviar@gmail.com>
2021-07-22 14:50:12 -0700

    Error handling and minor styling

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -1,22 +1,26 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Redirect, NavLink } from 'react-router-dom';
-import { signUp } from '../../store/session';
-import galaxySky from "../../assets/galaxySkyRight.png"
-import styles from './SignUpForm.module.css';
+import { Redirect, NavLink } from "react-router-dom";
+import { signUp } from "../../store/session";
+import galaxySky from "../../assets/galaxySkyRight.png";
+import styles from "./SignUpForm.module.css";
 
 const SignUpForm = () => {
   const dispatch = useDispatch();
-  const user = useSelector(state => state.session.user)
+  const user = useSelector((state) => state.session.user);
+  const [errors, setErrors] = useState([]);
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [repeatPassword, setRepeatPassword] = useState("");
+  const [match, setMatch] = useState(true);
 
   const onSignUp = async (e) => {
     e.preventDefault();
     if (password === repeatPassword) {
       const data = await dispatch(signUp(username, email, password));
+    } else {
+      setErrors(["Passwords must match"]);
     }
   };
 
@@ -36,6 +40,14 @@ const SignUpForm = () => {
     setRepeatPassword(e.target.value);
   };
 
+  useEffect(() => {
+    if (password !== repeatPassword) {
+      setMatch(false);
+    } else {
+      setMatch(true);
+    }
+  }, [password, repeatPassword]);
+
   if (user) {
     return <Redirect to="/" />;
   }
@@ -49,6 +61,13 @@ const SignUpForm = () => {
         <div className={styles.signUpFormTitle}>
           <h1>Make Your Money Move</h1>
         </div>
+        {errors.length > 0 ? (
+          <div className={styles.errorDiv}>
+            {errors.map((error) => (
+              <div>{error}</div>
+            ))}
+          </div>
+        ) : null}
         <form onSubmit={onSignUp}>
           <div>
             <label>User Name</label>
@@ -86,30 +105,35 @@ const SignUpForm = () => {
               value={repeatPassword}
               required={true}
             ></input>
+            <div> {match ? "" : "Passwords do not match"}</div>
           </div>
           <div className={styles.signupButtons}>
-          <button type="submit">Sign Up</button>
+            <button type="submit">Sign Up</button>
           </div>
           <div className={styles.accountAlready}>
-            <NavLink to='/login'>
-              <p>Already have an account?  Please click here!</p>
+            <NavLink to="/login">
+              <p>Already have an account? Please click here!</p>
             </NavLink>
           </div>
         </form>
         <div className={styles.signupDisclaimer}>
-          <p>All investments involve risk, including the possible loss of principal. Investors should consider their investment objectives and risks carefully before investing.
-
+          <p>
+            All investments involve risk, including the possible loss of
+            principal. Investors should consider their investment objectives and
+            risks carefully before investing.
           </p>
           <br />
           <p>
-            Commission-free trading means $0 commission trading on self-directed individual cash or margin brokerage accounts that trade U.S. listed securities via mobile devices or web. Keep in mind, other fees such as trading (non-commission) fees, wire transfer fees, and paper statement fees may apply to your brokerage account.
-            Securities trading offered through spaceXchange. 
+            Commission-free trading means $0 commission trading on self-directed
+            individual cash or margin brokerage accounts that trade U.S. listed
+            securities via mobile devices or web. Keep in mind, other fees such
+            as trading (non-commission) fees, wire transfer fees, and paper
+            statement fees may apply to your brokerage account. Securities
+            trading offered through spaceXchange.
           </p>
         </div>
       </div>
-      <div className={styles.spacerDiv}>
-
-      </div>
+      <div className={styles.spacerDiv}></div>
       <div className={styles.signupImgContainer}>
         <img src={galaxySky} />
       </div>
