77cda41bc3318676a048419b0cbd6f765a18f410
Zaviar Brown <brown.zaviar@gmail.com>
2021-07-29 11:59:32 -0700

    Styling splash

--- react-app/src/components/Splash/Splash.js
+++ react-app/src/components/Splash/Splash.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import NavBar from '../NavBar/NavBar'
-import Githubs from '../Githubs/Githubs';
-import galaxy from '../../assets/galaxyPixel.png'
-import styles from './Splash.module.css'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import NavBar from "../NavBar/NavBar";
+import Githubs from "../Githubs/Githubs";
+import galaxy from "../../assets/SpaceArt.png";
+import styles from "./Splash.module.css";
 
 export default function Splash() {
-
   return (
     <>
       <div className={styles.navContainer}>
@@ -17,14 +16,24 @@ export default function Splash() {
           <div className={styles.mainContentDivider}>
             <div className={styles.mainContentLeft}>
               <div className={styles.leftTitle}>
-                <h1>SPACE Investing <br /> For Everyone!</h1>
+                <h1>
+                  SPACE Investing <br /> For Everyone!
+                </h1>
               </div>
-              <h3>Commission-free investing, plus the tools you need to put your money in motion. </h3>
-                <div className={styles.signupButton}>
-                  <NavLink to="/sign-up" className={styles.navsignup} exact={true} activeClassName="active">
-                    Sign Up
-                  </NavLink>
-                </div>
+              <h3>
+                Commission-free investing, plus the tools you need to put your
+                money in motion.{" "}
+              </h3>
+              <div className={styles.signupButton}>
+                <NavLink
+                  to="/sign-up"
+                  className={styles.navsignup}
+                  exact={true}
+                  activeClassName="active"
+                >
+                  Sign Up
+                </NavLink>
+              </div>
             </div>
             <div className={styles.mainContentRight}>
               <img src={galaxy} />
